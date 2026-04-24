518ec0fc7c31eafc5e8e9c727a47f68cb769d0ac
LekeneCedric <lekenelucprofessionel@gmail.com>
2026-04-24 16:49:46 +0100

    test

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from the AutoDeploy Test Laboratory 🚀")
+	fmt.Fprintf(w, "Hello from the AutoDeploy Test Labor 🚀")
 }
 
 func main() {
